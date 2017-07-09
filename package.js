3215d73f4628d2cf8f3ecf39781d433837be791f
Mitar <mitar.git@tnode.com>
2017-07-08 22:48:43 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'peerlibrary:computed-field',
   summary: "Reactively computed field for Meteor",
-  version: '0.6.1',
+  version: '0.7.0',
   git: 'https://github.com/peerlibrary/meteor-computed-field.git'
 });
 
